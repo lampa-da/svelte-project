ec76b18f52616325da8f98d003a16ce8f54a8c9e
Kate Sv <radistkatya@gmail.com>
2022-05-20 22:01:16 -0700

    form added

---
+++ src/components/Form.svelte
@@ -0,0 +1,47 @@
+<script>
+  let email
+  let confirmEmail
+  let password
+  let confirmPassword
+  let showPassword = false
+
+  const handleSubmit = () => {
+    console.log(email, confirmEmail, password, confirmPassword, showPassword)
+  }
+</script>
+
+<main>
+  <form class="form" on:submit|preventDefault={handleSubmit}>
+    <p>Create an account</p>
+    <button>Sign up with Google</button>
+    <div class="orSection">
+      <hr />
+      or
+      <hr />
+    </div>
+    <label>
+      Email address
+      <input type="email" name="email" id="email" placeholder="Email address" bind:value={email} />
+    </label>
+    <label>
+      Confirm email address
+      <input type="email" name="confirmEmail" placeholder="Confirm email address" bind:value={confirmEmail} />
+    </label>
+    <label>
+      Password (at least 8 caracteres)
+      <input type="password" name="password" placeholder="Password" bind:value={password} />
+    </label>
+    <label>
+      Confirm Password
+      <input type="password" name="confirmPassword" placeholder="Confirm Password" bind:value={confirmPassword} />
+    </label>
+    <label>
+      <input type="checkbox" bind:checked={showPassword} />
+      Show Password
+    </label>
+    <button>Sign Up</button>
+  </form>
+</main>
+
+<style>
+</style>
